309a893af57ab21ca966d99fa8dcbf3e5212f04f
Prashant <123720428+prashant334434@users.noreply.github.com>
2024-09-13 11:31:17 +0530

    icon import and header design

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,9 +1,6 @@
 import Back from './back.svg';
-
 import GoogleIcon from './google.svg';
 import FBIcon from './facebook.svg';
-
-
 import Home from './Tabicon/home.svg';
 import Home_Fill from './Tabicon/home-fill.svg';
 import Profile from './Tabicon/account.svg';
@@ -12,7 +9,28 @@ import Search from './Tabicon/search.svg';
 import SearchFill from './Tabicon/search-fill.svg';
 import Network from './Tabicon/network.svg';
 import NetworkFill from './Tabicon/network-fill.svg';
-import Person from'./Tabicon/person.svg';
+import Person from './Tabicon/person.svg';
 import Add_Icon from './Tabicon/addBlue.svg';
-
-export {Add_Icon,Back, GoogleIcon, FBIcon,Home,Home_Fill,Profile,Profile_Fill,Search,SearchFill,Network,NetworkFill,Person};
+import Map_pin from './mapPin.svg';
+import Notification_Icon from './notification-bing.svg';
+import Chat_icon from './chat_icon.svg';
+import verified_blue from './verified_blue.svg';
+export {
+  Map_pin,
+  verified_blue,
+  Add_Icon,
+  Notification_Icon,
+  Chat_icon,
+  Back,
+  GoogleIcon,
+  FBIcon,
+  Home,
+  Home_Fill,
+  Profile,
+  Profile_Fill,
+  Search,
+  SearchFill,
+  Network,
+  NetworkFill,
+  Person,
+};
--- src/screens/DashboradScreen.tsx
+++ src/screens/DashboradScreen.tsx
@@ -1,11 +1,10 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '@reduxjs/toolkit/query';
+import {useSelector} from 'react-redux';
+import {RootState} from '@reduxjs/toolkit/query';
 
 import FastImage from '@d11/react-native-fast-image';
 export default function DashboradScreen() {
-
   const AppLocation = useSelector((state: RootState) => state.AppLocation);
 
   return (
@@ -16,15 +15,15 @@ export default function DashboradScreen() {
       <Text>{AppLocation.country}</Text>
       <Text>{AppLocation.placeID}</Text>
       <Text>{AppLocation.placeName}</Text>
-      <FastImage
-    resizeMode={FastImage.resizeMode.contain}
-    style={{ height: 200, width: 200 }}
-    source={{
-      headers: { Authorization: "someAuthToken" },
-      priority: FastImage.priority.normal,
-      uri: "https://unsplash.it/400/400?image=1",
-    }}
-  />
+      {/* <FastImage
+        resizeMode={FastImage.resizeMode.contain}
+        style={{height: 200, width: 200}}
+        source={{
+          headers: {Authorization: 'someAuthToken'},
+          priority: FastImage.priority.normal,
+          uri: 'https://unsplash.it/400/400?image=1',
+        }}
+      /> */}
     </View>
   );
 }
