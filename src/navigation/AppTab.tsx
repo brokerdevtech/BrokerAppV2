969efdafd5ba5f5435831e1ec92cc22ff21d0007
kanishkpuri1987 <kanishkpuri87@gmail.com>
2024-12-23 17:27:42 +0530

    add

--- src/navigation/AppTab.tsx
+++ src/navigation/AppTab.tsx
@@ -136,7 +136,7 @@ const AppTab: React.FC = () => {
       <Tab.Navigator
         initialRouteName="AppTabHome"
         screenOptions={({route}) => ({
-          // unmountOnBlur: true,
+           unmountOnBlur: true,
           headerShown: false,
           headerTitle: '',
           tabBarStyle: [
@@ -160,6 +160,8 @@ const AppTab: React.FC = () => {
           name="AppTabHome"
           component={HomePageStack}
           options={{
+            unmountOnBlur: true,
+
             tabBarIcon: ({focused}) => (
               <TabText
                 text={''}
