18979a243152df3edb4abb2cac7db76c28af9cca
Prashant <123720428+prashant334434@users.noreply.github.com>
2025-01-20 13:45:50 +0530

    bugs

--- index.js
+++ index.js
@@ -20,17 +20,14 @@ const tokenProvider: TokenOrProvider = async userId => {
     throw error;
   }
 };
-messaging().onMessage(async remoteMessage => {
-  if (Object.keys(remoteMessage.data).length === 0) {
+async function handleNotification(remoteMessage, isBackground = false) {
+  if (isBackground) {
     const channelId = await notifee.createChannel({
       id: 'app-messages',
       name: 'App Messages',
-      vibration: true, // Ensure vibration is enabled
-      importance: AndroidImportance.HIGH,
     });
 
     const data = {};
-    console.log('setBackgroundMessageHandler', remoteMessage);
     await notifee.displayNotification({
       android: {
         channelId,
@@ -38,15 +35,24 @@ messaging().onMessage(async remoteMessage => {
           id: 'default',
         },
       },
-
+      ios: {
+        foregroundPresentationOptions: {
+          badge: true,
+          sound: true,
+          banner: true,
+          list: true,
+        },
+      },
       body: remoteMessage.notification.body,
       data,
       title: remoteMessage.notification.title,
-      sound: 'default',
     });
-  } else {
+  }
+
+  if (Object.keys(remoteMessage.data).length > 0) {
     const messageId = remoteMessage.data?.id;
     if (!messageId) return;
+
     const chatClient = StreamChat.getInstance(chatApiKey);
     const user = {
       id: remoteMessage.data?.receiver_id,
@@ -55,19 +61,19 @@ messaging().onMessage(async remoteMessage => {
 
     await chatClient._setToken(user, token);
     const message = await chatClient.getMessage(messageId);
-    // console.log("setBackgroundMessageHandler");
-    const channelId = await notifee.createChannel({
-      id: 'chat-messages',
-      name: 'Chat Messages',
-    });
 
-    if (message.message.user?.name && message.message.text) {
+    if (message.message.user?.name && message.message.text && isBackground) {
+      const channelId = await notifee.createChannel({
+        id: 'chat-messages',
+        name: 'Chat Messages',
+      });
+
       const {stream, ...rest} = remoteMessage.data ?? {};
       const data = {
         ...rest,
-        ...(stream ?? {}), // extract and merge stream object if present
+        ...(stream ?? {}),
       };
-      //  console.log("setBackgroundMessageHandler");
+
       await notifee.displayNotification({
         android: {
           channelId,
@@ -75,79 +81,23 @@ messaging().onMessage(async remoteMessage => {
             id: 'default',
           },
         },
-
         body: message.message.text,
         data: data,
         title: 'New message from ' + message.message.user.name,
-
         sound: 'default',
       });
     }
   }
+}
+
+messaging().onMessage(async remoteMessage => {
+  console.log(remoteMessage, 'onMessage');
+
+  await handleNotification(remoteMessage, false);
 });
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-  // console.log(remoteMessage);
-
-  if (Object.keys(remoteMessage.data).length === 0) {
-    const channelId = await notifee.createChannel({
-      id: 'app-messages',
-      name: 'App Messages',
-    });
-
-    const data = {};
-    // console.log("setBackgroundMessageHandler");
-    await notifee.displayNotification({
-      android: {
-        channelId,
-        pressAction: {
-          id: 'default',
-        },
-      },
-
-      body: remoteMessage.notification.body,
-      data,
-      title: remoteMessage.notification.title,
-    });
-  } else {
-    const messageId = remoteMessage.data?.id;
-    if (!messageId) return;
-    const chatClient = StreamChat.getInstance(chatApiKey);
-    const user = {
-      id: remoteMessage.data?.receiver_id,
-    };
-    let token = await tokenProvider(remoteMessage.data?.receiver_id);
-
-    await chatClient._setToken(user, token);
-    const message = await chatClient.getMessage(messageId);
-    // console.log("setBackgroundMessageHandler");
-    const channelId = await notifee.createChannel({
-      id: 'chat-messages',
-      name: 'Chat Messages',
-    });
-
-    if (message.message.user?.name && message.message.text) {
-      const {stream, ...rest} = remoteMessage.data ?? {};
-      const data = {
-        ...rest,
-        ...(stream ?? {}), // extract and merge stream object if present
-      };
-      //  console.log("setBackgroundMessageHandler");
-      await notifee.displayNotification({
-        android: {
-          channelId,
-          pressAction: {
-            id: 'default',
-          },
-        },
-
-        body: message.message.text,
-        data: data,
-        title: 'New message from ' + message.message.user.name,
-
-        sound: 'default',
-      });
-    }
-  }
+  console.log('setBackgroundMessageHandler', remoteMessage);
+  await handleNotification(remoteMessage, true);
 });
 AppRegistry.registerComponent(appName, () => App);
