3902712acb1d93ddb9d66c94af652ba2d5a3d153
Prashant <123720428+prashant334434@users.noreply.github.com>
2025-02-03 16:35:01 +0530

    subs

--- src/hooks/useApiPagingWithtotalRequest.ts
+++ src/hooks/useApiPagingWithtotalRequest.ts
@@ -3,8 +3,8 @@ import {ApiResponse} from '../../BrokerAppCore/services/new/ApiResponse'; // Ass
 
 export const useApiPagingWithtotalRequest = <T, P extends any[]>(
   apiFunction: (...args: [...P, number, number]) => Promise<ApiResponse<T>>,
-  setLoading?: (loading: boolean) => void, 
-  ApipageSize:number=5
+  setLoading?: (loading: boolean) => void,
+  ApipageSize: number = 5,
   // Loading function passed from outside
 ) => {
   const [data, setData] = useState<T | null>(null);
@@ -14,100 +14,105 @@ export const useApiPagingWithtotalRequest = <T, P extends any[]>(
   const [error, setError] = useState<string | null>(null);
   const [currentPage, setCurrentPage] = useState<number>(1); // Track the current page
   const [pageSize, setPageSize] = useState<number>(ApipageSize); // Default page size
-  const [hasMore, setHasMore] = useState<boolean>(true); 
+  const [hasMore, setHasMore] = useState<boolean>(true);
   // Function to trigger the API call
   const isMounted = useRef(true); // Flag to track if the component is mounted
-  const paramsRef = useRef<P | null>(null); 
+  const paramsRef = useRef<P | null>(null);
 
-  const execute =  useCallback(async (...params: P) => {
-
-    // Call the external loading function, if provided
-    paramsRef.current = params; // Store the parameters for future use
-    if (setLoading) {
-      setLoading(true);
-    }
-
-    setError(null);
-    setCurrentPage(1); // Reset to first page for initial load
-    setHasMore(true); // Reset hasMore for fresh load
-    try {
-   
-      const response = await apiFunction(...params, 1, pageSize);
-      if (isMounted.current) { // Only update state if the component is mounted
-      if (setLoading) {
-        setLoading(false);
-      }
-
-      if (!response.success) {
-        setError(response.message || 'An error occurred');
-        setStatus(response.status || 500);
-      } else {
-      
-        setData(response.data.data || null);
-        settotalPages(response.data.totalPages || null)
-        setrecordCount(response.data.recordCount || null)
-        setStatus(response.status || 200);
-      }
-   }
-   } catch (error: any) {
-    if (isMounted.current) {
-      if (setLoading) {
-        setLoading(false);
-      }
-
-      setError('An unexpected error occurred');
-      setStatus(500);}
-    }
-  }, [apiFunction, pageSize, setLoading]);
-
-
-
-
-  const loadMore  = useCallback(async (...params: P) => {
-    if (hasMore) {
+  const execute = useCallback(
+    async (...params: P) => {
+      // Call the external loading function, if provided
+      paramsRef.current = params; // Store the parameters for future use
       if (setLoading) {
         setLoading(true);
       }
 
+      setError(null);
+      setCurrentPage(1); // Reset to first page for initial load
+      setHasMore(true); // Reset hasMore for fresh load
       try {
-        // Use stored params along with current page and page size
-        const response = await apiFunction(...params, currentPage + 1, pageSize);
+        const response = await apiFunction(...params, 1, pageSize);
+        if (isMounted.current) {
+          // Only update state if the component is mounted
+          if (setLoading) {
+            setLoading(false);
+          }
 
-     
-        if (isMounted.current) { 
-        if (!response.success) {
-          setError(response.message || 'An error occurred');
-          setStatus(response.status || 500);
-        } else {
-        
-          if(response?.data.data!=null && response?.data.data.length > 0)
-       {   setData((prevData) => [...prevData, ...(response.data.data || [])]);
-          setStatus(response.status || 200);
-          setCurrentPage((prevPage) => prevPage + 1); // Increment the page count
-      
-       }
-       else{
-
-        setHasMore(false); 
-       }
+          if (!response.success) {
+            setError(response.message || 'An error occurred');
+            setStatus(response.status || 500);
+          } else {
+            setData(response.data.data || null);
+            settotalPages(response.data.totalPages || null);
+            setrecordCount(response.data.recordCount || null);
+            setStatus(response.status || 200);
+          }
         }
-
-         }   // Check if more data is available
-      
-        
       } catch (error: any) {
-        if (isMounted.current) { 
+        if (isMounted.current) {
+          if (setLoading) {
+            setLoading(false);
+          }
 
-        setError('An unexpected error occurred');
-        setStatus(500);}
-      }
-      finally {
-        if (isMounted.current && setLoading) {
-          setLoading(false);
+          setError('An unexpected error occurred');
+          setStatus(500);
         }
       }
-    }
-  }, [apiFunction, currentPage, hasMore, pageSize, status, setLoading]);
+    },
+    [apiFunction, pageSize, setLoading],
+  );
+
+  const loadMore = useCallback(
+    async (...params: P) => {
+      if (hasMore) {
+        if (setLoading) {
+          setLoading(true);
+        }
+
+        try {
+          // Use stored params along with current page and page size
+          console.log(...params, currentPage + 1, pageSize, 'aparam');
+          const response = await apiFunction(
+            ...params,
+            currentPage + 1,
+            pageSize,
+          );
+
+          if (isMounted.current) {
+            if (!response.success) {
+              setError(response.message || 'An error occurred');
+              setStatus(response.status || 500);
+            } else {
+              console.log(response?.data.data, 'DataFormHook');
+              if (
+                response?.data.data != null &&
+                response?.data.data.length > 0
+              ) {
+                setData(prevData => [
+                  ...prevData,
+                  ...(response.data.data || []),
+                ]);
+                setStatus(response.status || 200);
+                setCurrentPage(prevPage => prevPage + 1); // Increment the page count
+              } else {
+                setHasMore(false);
+              }
+            }
+          } // Check if more data is available
+        } catch (error: any) {
+          if (isMounted.current) {
+            setError('An unexpected error occurred');
+            setStatus(500);
+          }
+        } finally {
+          if (isMounted.current && setLoading) {
+            setLoading(false);
+          }
+        }
+      }
+    },
+    [apiFunction, currentPage, hasMore, pageSize, status, setLoading],
+  );
 
   const pageSize_Set = useCallback((newPageSize: number) => {
     setPageSize(newPageSize);
@@ -127,8 +132,7 @@ export const useApiPagingWithtotalRequest = <T, P extends any[]>(
       if (setLoading) setLoading(false); // Reset loading state when unmounted
     };
   }, [setLoading]);
-  const setData_Set  = (newData: T | null) => {
-   
+  const setData_Set = (newData: T | null) => {
     // console.log('oldData');
     // console.log(data);
     // let newd=  data.map((item) =>
@@ -136,7 +140,20 @@ export const useApiPagingWithtotalRequest = <T, P extends any[]>(
     // )
     // setData(newd);
     // console.log(newd);
-
   };
-  return {data, status, error, execute,loadMore,pageSize_Set,currentPage_Set,hasMore_Set,totalPages,recordCount,hasMore,setData_Set};
+  // console.log(data, 'Hook');
+  return {
+    data,
+    status,
+    error,
+    execute,
+    loadMore,
+    pageSize_Set,
+    currentPage_Set,
+    hasMore_Set,
+    totalPages,
+    recordCount,
+    hasMore,
+    setData_Set,
+  };
 };
--- src/screens/SubscriptionPlan.tsx
+++ src/screens/SubscriptionPlan.tsx
@@ -23,6 +23,7 @@ import {subsriptionPlanApi} from '../../BrokerAppCore/services/new/Subscription'
 import NoDataFoundScreen from '../sharedComponents/NoDataFoundScreen';
 import {formatDate} from '../constants/constants';
 import PlanDetailsScreen from '../sharedComponents/PlanDetailScreen';
+import {FlashList} from '@shopify/flash-list';
 
 const SubscriptionPlan = ({route}) => {
   const user = useSelector((state: RootState) => state.user.user);
@@ -38,306 +39,246 @@ const SubscriptionPlan = ({route}) => {
   const closeModal = useCallback(() => {
     setSelectedItem(null);
   }, []);
+  const flatListRef = useRef(null);
+  const [isInfiniteLoading, setInfiniteLoading] = useState(false);
 
-  const renderItem = useCallback(
-    ({item}) => {
-      const limits = item.limits ? JSON.parse(item.limits) : {};
-      const additionalBenefits = item.additionalBenifits
-        ? JSON.parse(item.additionalBenifits)
-        : {};
-      const AdsCategoryMap = {
-        1: 'Marquee',
-        2: 'Carousel',
-      };
+  const planType =
+    activeTab === 'General'
+      ? 1
+      : activeTab === 'Ads'
+      ? 2
+      : activeTab === 'Space ads'
+      ? 3
+      : 0;
+  const {
+    data: Plandata,
+    status: Planstatus,
+    execute: Planexecute,
 
-      // Map for SpaceCategory
-      const SpaceCategoryMap = {
-        1: 'Newly Launched',
-        2: 'Brand Associated',
-        3: 'New in Property',
-        4: 'New in Car',
-      };
-      const hasLimits = Object.keys(limits).some(key =>
-        Array.isArray(limits[key])
-          ? limits[key].length > 0
-          : limits[key] !== undefined,
-      );
+    loadMore: PlanloadMore,
+    hasMore: PlanHasMore,
+  } = useApiPagingWithtotalRequest(subsriptionPlanApi, setInfiniteLoading, 4);
 
-      const hasAdditionalBenefits = Object.keys(additionalBenefits).some(key =>
-        Array.isArray(additionalBenefits[key])
-          ? additionalBenefits[key].length > 0
-          : additionalBenefits[key] !== undefined,
-      );
-      return (
-        <TouchableOpacity style={styles.card}>
-          <View style={styles.row}>
-            <ZText type={'S22'} style={styles.price}>
-              {item?.currency + ' '}
-              {item.price}
+  const getList = useCallback(async () => {
+    try {
+      setInfiniteLoading(false); // Reset loading state
+      await Planexecute(planType);
+    } catch (error) {
+      console.error('Error fetching plans:', error);
+      setInfiniteLoading(false);
+    }
+  }, [planType, Planexecute]);
+
+  useEffect(() => {
+    getList();
+  }, [planType]);
+  const loadMorePage = useCallback(async () => {
+    if (!isInfiniteLoading && PlanHasMore && Plandata?.length > 0) {
+      try {
+        setInfiniteLoading(true);
+        await PlanloadMore(planType);
+      } catch (error) {
+        console.error('Error loading more plans:', error);
+      } finally {
+        setInfiniteLoading(false);
+      }
+    }
+  }, [isInfiniteLoading, PlanHasMore, Plandata, PlanloadMore, planType]);
+  const renderItem = ({item}) => {
+    const limits = item.limits ? JSON.parse(item.limits) : {};
+    const additionalBenefits = item.additionalBenifits
+      ? JSON.parse(item.additionalBenifits)
+      : {};
+    const AdsCategoryMap = {
+      1: 'Marquee',
+      2: 'Carousel',
+    };
+
+    // Map for SpaceCategory
+    const SpaceCategoryMap = {
+      1: 'Newly Launched',
+      2: 'Brand Associated',
+      3: 'New in Property',
+      4: 'New in Car',
+    };
+    const hasLimits = Object.keys(limits).some(key =>
+      Array.isArray(limits[key])
+        ? limits[key].length > 0
+        : limits[key] !== undefined,
+    );
+
+    const hasAdditionalBenefits = Object.keys(additionalBenefits).some(key =>
+      Array.isArray(additionalBenefits[key])
+        ? additionalBenefits[key].length > 0
+        : additionalBenefits[key] !== undefined,
+    );
+    return (
+      <TouchableOpacity style={styles.card}>
+        <View style={styles.row}>
+          <ZText type={'S22'} style={styles.price}>
+            {item?.currency + ' '}
+            {item.price}
+          </ZText>
+          <View>
+            <ZText type={'R14'} style={styles.label}>
+              Validity
             </ZText>
-            <View>
-              <ZText type={'R14'} style={styles.label}>
-                Validity
-              </ZText>
-              <ZText type={'S16'} style={styles.value}>
-                {item.validityValue}{' '}
-                {item.validityType === 1 ? 'Days' : 'Hours'}
-              </ZText>
-            </View>
-            <View>
-              {Object.keys(limits).map(key => {
-                if (key === 'Ads' && Array.isArray(limits[key])) {
-                  const firstAd = limits[key][0];
-                  if (firstAd) {
-                    return (
-                      <React.Fragment key={`${key}-container`}>
-                        <Text style={styles.label}>{key}</Text>
-                        <Text style={styles.value}>{firstAd.AdCount}</Text>
-                      </React.Fragment>
-                    );
-                  }
-                } else if (key === 'SpaceAd' && Array.isArray(limits[key])) {
-                  const firstAd = limits[key][0];
-                  if (firstAd) {
-                    return (
-                      <React.Fragment key={`${key}-container`}>
-                        <Text style={styles.label}>{key}</Text>
-                        <Text style={styles.value}>{firstAd.AdCount}</Text>
-                      </React.Fragment>
-                    );
-                  }
-                } else {
+            <ZText type={'S16'} style={styles.value}>
+              {item.validityValue} {item.validityType === 1 ? 'Days' : 'Hours'}
+            </ZText>
+          </View>
+          <View>
+            {Object.keys(limits).map(key => {
+              if (key === 'Ads' && Array.isArray(limits[key])) {
+                const firstAd = limits[key][0];
+                if (firstAd) {
                   return (
                     <React.Fragment key={`${key}-container`}>
                       <Text style={styles.label}>{key}</Text>
-                      <Text style={styles.value}>{limits[key]}</Text>
+                      <Text style={styles.value}>{firstAd.AdCount}</Text>
                     </React.Fragment>
                   );
                 }
-              })}
-            </View>
+              } else if (key === 'SpaceAd' && Array.isArray(limits[key])) {
+                const firstAd = limits[key][0];
+                if (firstAd) {
+                  return (
+                    <React.Fragment key={`${key}-container`}>
+                      <Text style={styles.label}>{key}</Text>
+                      <Text style={styles.value}>{firstAd.AdCount}</Text>
+                    </React.Fragment>
+                  );
+                }
+              } else {
+                return (
+                  <React.Fragment key={`${key}-container`}>
+                    <Text style={styles.label}>{key}</Text>
+                    <Text style={styles.value}>{limits[key]}</Text>
+                  </React.Fragment>
+                );
+              }
+            })}
           </View>
-          <View style={styles.divider} />
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignItems: 'flex-end',
-            }}>
-            <View type={'B26'} style={styles.details}>
-              {hasLimits && (
-                <>
-                  <ZText type={'R12'} style={styles.detailText}>
-                    Limits : {item.limits.post}
-                  </ZText>
-                  {Object.keys(limits).map((key, index) => {
-                    const benefit = limits[key];
-                    if (Array.isArray(benefit)) {
-                      return benefit.map((item, idx) => {
-                        const categoryName =
-                          key === 'Ads'
-                            ? AdsCategoryMap[item.Category]
-                            : SpaceCategoryMap[item.Category];
+        </View>
+        <View style={styles.divider} />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'flex-end',
+          }}>
+          <View type={'B26'} style={styles.details}>
+            {hasLimits && (
+              <>
+                <ZText type={'R12'} style={styles.detailText}>
+                  Limits : {item.limits.post}
+                </ZText>
+                {Object.keys(limits).map((key, index) => {
+                  const benefit = limits[key];
+                  if (Array.isArray(benefit)) {
+                    return benefit.map((item, idx) => {
+                      const categoryName =
+                        key === 'Ads'
+                          ? AdsCategoryMap[item.Category]
+                          : SpaceCategoryMap[item.Category];
 
-                        return (
-                          <ZText
-                            key={`${key}-${idx}`}
-                            type={'R12'}
-                            style={styles.value}>
-                            {key}: {item.AdCount} | {categoryName} | Validity:
-                            {item.Validity} days
-                          </ZText>
-                        );
-                      });
-                    }
-                    return (
-                      <ZText key={index} type={'R12'} style={styles.value}>
-                        {key}: {additionalBenefits[key]}
-                      </ZText>
-                    );
-                  })}
-                </>
-              )}
-              {hasAdditionalBenefits && (
-                <>
-                  <ZText
-                    type={'R12'}
-                    style={[styles.detailText, {marginTop: 8}]}>
-                    Additional Benefits:
-                  </ZText>
-                  {Object.keys(additionalBenefits).map((key, index) => {
-                    const benefit = additionalBenefits[key];
-                    if (Array.isArray(benefit)) {
-                      return benefit.map((item, idx) => {
-                        const categoryName =
-                          key === 'Ads'
-                            ? AdsCategoryMap[item.Category]
-                            : SpaceCategoryMap[item.Category];
+                      return (
+                        <ZText
+                          key={`${key}-${idx}`}
+                          type={'R12'}
+                          style={styles.value}>
+                          {key}: {item.AdCount} | {categoryName} | Validity:
+                          {item.Validity} days
+                        </ZText>
+                      );
+                    });
+                  }
+                  return (
+                    <ZText key={index} type={'R12'} style={styles.value}>
+                      {key}: {additionalBenefits[key]}
+                    </ZText>
+                  );
+                })}
+              </>
+            )}
+            {hasAdditionalBenefits && (
+              <>
+                <ZText type={'R12'} style={[styles.detailText, {marginTop: 8}]}>
+                  Additional Benefits:
+                </ZText>
+                {Object.keys(additionalBenefits).map((key, index) => {
+                  const benefit = additionalBenefits[key];
+                  if (Array.isArray(benefit)) {
+                    return benefit.map((item, idx) => {
+                      const categoryName =
+                        key === 'Ads'
+                          ? AdsCategoryMap[item.Category]
+                          : SpaceCategoryMap[item.Category];
 
-                        return (
-                          <ZText
-                            key={`${key}-${idx}`}
-                            type={'R12'}
-                            style={styles.value}>
-                            {key}: {item.AdCount} | {categoryName} | Validity:
-                            {item.Validity} days
-                          </ZText>
-                        );
-                      });
-                    }
-                    return (
-                      <ZText key={index} type={'R12'} style={styles.value}>
-                        {key}: {additionalBenefits[key]}
-                      </ZText>
-                    );
-                  })}
-                </>
-              )}
-            </View>
-            <TouchableOpacity
-              style={{paddingHorizontal: 10}}
-              onPress={() => handlePresentModalPress(item)}>
-              <ZText
-                type={'S12'}
-                color={Color.primary}
-                style={{textDecorationLine: 'underline'}}>
-                Read more
-              </ZText>
-            </TouchableOpacity>
+                      return (
+                        <ZText
+                          key={`${key}-${idx}`}
+                          type={'R12'}
+                          style={styles.value}>
+                          {key}: {item.AdCount} | {categoryName} | Validity:
+                          {item.Validity} days
+                        </ZText>
+                      );
+                    });
+                  }
+                  return (
+                    <ZText key={index} type={'R12'} style={styles.value}>
+                      {key}: {additionalBenefits[key]}
+                    </ZText>
+                  );
+                })}
+              </>
+            )}
           </View>
-        </TouchableOpacity>
-      );
-    },
-    [handlePresentModalPress],
-  );
-
-  const renderRecommendedplans = useCallback(
-    plantype => {
-      const flatListRef = useRef(null);
-      const [isInfiniteLoading, setInfiniteLoading] = useState(false);
-     
-      const  {
-        data: Plandata,
-        status: Planstatus,
-        execute: Planexecute,
-        hasMore,
-        loadMore: PlanloadMore,
-        pageSize_Set,currentPage_Set,hasMore_Set,totalPages,recordCount,setData_Set
-
-
-
-      } = useApiPagingWithtotalRequest(
-        subsriptionPlanApi,
-        setInfiniteLoading,
-        6,
-      );
-
-      const getList = async () => {
-        try {
-          await  hasMore_Set(true)
-          await Planexecute(plantype);
-        } catch (error) {
-          console.error('Error fetching plans:', error);
-        }
-      };
-
-      useEffect(() => {
-        // Plandata=null;
-        //setData_Set(null)
-     
-        
-        getList();
-      }, [plantype]);
-
-      useEffect(() => {
-        if (Planstatus == 200) {
-        console.log('Plan',Plandata);
-        }
-      }, [Plandata, Planstatus]);
-
-      const loadMorepage = async () => {
-        if (!isInfiniteLoading) {
-          try {
-            await PlanloadMore(plantype);
-          } catch (error) {
-            console.error('Error loading more pages:', error);
-          }
-        }
-      };
-
-      return (
-        
-        <FlatList
-          data={Plandata}
-          renderItem={renderItem}
-          ref={flatListRef}
-          getItemLayout={(data, index) => ({
-            length: 560,
-            offset: 560 * index,
-            index,
-          })}
-          initialNumToRender={2}
-          maxToRenderPerBatch={4}
-        
-          showsHorizontalScrollIndicator={false}
-          showsVerticalScrollIndicator={false}
-          onEndReachedThreshold={0.6}
-          onEndReached={loadMorepage}
-          contentContainerStyle={{paddingBottom: 100}}
-          ListFooterComponent={
-            isInfiniteLoading ? (
-              <ActivityIndicator
-                size="large"
-                color="#0000ff"
-                style={styles.loader}
-              />
-            ) : null
-          }
-          removeClippedSubviews={false}
-          // ListEmptyComponent={() =>
-          //   plans.length === 0 ? <NoDataFoundScreen /> : null
-          // }
-        />
-      );
-    },
-    [renderItem],
-  );
-
+          <TouchableOpacity
+            style={{paddingHorizontal: 10}}
+            onPress={() => handlePresentModalPress(item)}>
+            <ZText
+              type={'S12'}
+              color={Color.primary}
+              style={{textDecorationLine: 'underline'}}>
+              Read more
+            </ZText>
+          </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+  console.log(Plandata);
   return (
     <View style={{flex: 1}}>
       <View style={styles.tabContainer}>
-        <TouchableOpacity
-          style={[
-            styles.tabButton,
-            activeTab === 'General' && styles.activeTab,
-          ]}
-          onPress={() => setActiveTab('General')}>
-          <ZText type={'R16'} style={styles.tabText}>
-            General
-          </ZText>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[styles.tabButton, activeTab === 'Ads' && styles.activeTab]}
-          onPress={() => setActiveTab('Ads')}>
-          <ZText type={'R16'} style={styles.tabText}>
-            Ads
-          </ZText>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[
-            styles.tabButton,
-            activeTab === 'Space ads' && styles.activeTab,
-          ]}
-          onPress={() => setActiveTab('Space ads')}>
-          <ZText type={'R16'} style={styles.tabText}>
-            Space ads
-          </ZText>
-        </TouchableOpacity>
+        {['General', 'Ads', 'Space ads'].map(tab => (
+          <TouchableOpacity
+            key={tab}
+            style={[styles.tabButton, activeTab === tab && styles.activeTab]}
+            onPress={() => setActiveTab(tab)}>
+            <ZText type={'R16'} style={styles.tabText}>
+              {tab}
+            </ZText>
+          </TouchableOpacity>
+        ))}
       </View>
-     
-      {activeTab === 'General' && renderRecommendedplans(1)}
-      {activeTab === 'Ads' && renderRecommendedplans(2)}
-      {activeTab === 'Space ads' && renderRecommendedplans(3)}
+      <FlatList
+        data={Plandata}
+        renderItem={renderItem}
+        ref={flatListRef}
+        onEndReachedThreshold={0.3}
+        onEndReached={loadMorePage}
+        contentContainerStyle={{paddingBottom: 100}}
+        ListFooterComponent={
+          isInfiniteLoading ? (
+            <ActivityIndicator size="large" color={Color.primary} />
+          ) : null
+        }
+        removeClippedSubviews={false}
+        ListEmptyComponent={() => (Plandata ? <NoDataFoundScreen /> : null)}
+      />
 
       <PlanDetailsScreen
         ref={commentSheetRef}
